3305627cbafc2427b44dea82b80601d58c689451
Ubuntu <ubuntu@ip-172-31-23-56.us-east-2.compute.internal>
2018-11-08 22:54:50 +0000

    socket

--- sock.py
+++ sock.py
@@ -4,7 +4,10 @@ s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host='snoopy.mpi-inf.mpg.de'
 port=666
 s.connect((host,port))
+s.send("HELLO.\n")
 msg=s.recv(1024)
+print msg.decode('ascii')
+s.send(".\n")
 s.close()
-print(msg.decode('ascii'))
+
 
