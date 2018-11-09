cba3d0f3800ed24c896012d79d5478db2c4a2fe5
Ubuntu <ubuntu@ip-172-31-23-56.us-east-2.compute.internal>
2018-11-09 18:23:40 +0000

    socket

--- sock.py
+++ sock.py
@@ -3,11 +3,16 @@ import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 host='snoopy.mpi-inf.mpg.de'
 port=666
+i=0
 s.connect((host,port))
-s.send("HELLO.\n")
-msg=s.recv(1024)
-print msg.decode('ascii')
-s.send(".\n")
+s.send(b"HELLO.\n")
+s.send(b"DOWNLOAD.\n")
+while 1 :
+            msg=s.recv(10)
+            print(msg)      
+            if msg=='b': 
+             break
+                   
 s.close()
 
 
