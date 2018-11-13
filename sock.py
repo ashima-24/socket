67bc7aa02e17d642a490c99cca68273a5e934d52
ashima-24 <jindal241995@gmail.com>
2018-11-13 23:23:36 +0100

    sock

--- sock.py
+++ sock.py
@@ -9,24 +9,21 @@ s.send(b"HELLO.\n")
 s.send(b"DOWNLOAD.\n:")
 data=b""
 m=0
-k=0
 with open ('abc.txt','wb') as f:
     while 1:
         msg=s.recv(1024)
         if not msg:
             break
         else:
-            #            print(msg)
-            data+=msg
-            pos=pos+1
+             print(msg)
+             data+=msg
+             pos=pos+1
         if pos>=5:
                  print(msg)
                  m=data.find(b"TOKEN")
                  if m==62587:
                              break
                  f.write(msg)
-print(m)
-print(pos)
 s.close()
 
 
