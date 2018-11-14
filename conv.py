dd0e0e8f23340664a17c4677982114e53563adb2
ashima-24 <jindal241995@gmail.com>
2018-11-14 23:04:48 +0100

    done

--- conv.py
+++ conv.py
@@ -1,11 +1,17 @@
 import socket
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 HOST = 'snoopy.mpi-inf.mpg.de'
 PORT = 666
+
 s.connect((HOST, PORT))
+
 s.send(b"HELLO.\n")
 s.send(b"DOWNLOAD.\n:")
+
 data = b""
+
 while 1:
        msg = s.recv(1024)
        if not msg:
@@ -32,14 +38,17 @@ img1 = bytearray(t[0])
 img3 = img1.lstrip(b'\n')
 print(img3)   # raw bytes of image
 
+
 with open('CAT.jpeg', 'wb') as i:
-    i.write(img3)
+    i.write(img3)   # writing image in CAT.jpeg
+
 
 tok = t[1].split(b'\n')
 token = tok[0].split(b':')
-print(token[1])  # 32 bit token
+token=token[1].split()
+print(token[0])  # 32 bit token
 
 with open('TOKEN.txt', 'wb') as w:
-    w.write(token[1])
+    w.write(token[0]) # writing token in TOKEN.txt
 
 s.close()
