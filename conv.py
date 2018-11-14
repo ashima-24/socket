328e47971df1e1b816e168fe533023ebea37f0de
ashima-24 <jindal241995@gmail.com>
2018-11-14 23:30:44 +0100

    done  changes

--- conv.py
+++ conv.py
@@ -9,41 +9,38 @@ s.connect((HOST, PORT))
 s.send(b"HELLO.\n")
 s.send(b"DOWNLOAD.\n:")
 
-data = b""
-size=1024
-cat='CAT.jpeg'
-to='TOKEN.txt'
+DATA = b""
+SIZE=1024
+CAT='CAT.jpeg'
+TO='TOKEN.txt'
 
-while 1:
-       msg = s.recv(size)
+while True:
+       msg = s.recv(SIZE)
        if not msg:
                  break
        else:
-            data += msg
+            DATA += msg
 
-start = data.split(b'\n')
-helo = data.split(b'\n')[0]
-comand = data.split(b'\n')[1]
-fil = data.split(b'\n')[2]
-b = data.split(b'\n')[3]
+start = DATA.split(b'\n')
+helo = DATA.split(b'\n')[0]
+comand = DATA.split(b'\n')[1]
+fil = DATA.split(b'\n')[2]
+b = DATA.split(b'\n')[3]
 img = b.split()[1] # no of bytes of images
 
-H = data.split(b'BYTES')
+H = DATA.split(b'BYTES')
 t = H[1].split(b'TOKEN')
 
-img1 = bytearray(t[0])
-img3 = img1.lstrip(b'\n')  # raw bytes of image
+img3 = t[0].lstrip(b'\n')  # raw bytes of image
 
-
-with open(cat, 'wb') as i:
+with open(CAT, 'wb') as i:
     i.write(img3)   # writing image in CAT.jpeg
-
 
 tok = t[1].split(b'\n')
 token = tok[0].split(b':')
 token=token[1].split()  # 32 bit token
 
-with open(to, 'wb') as w:
+with open(TO, 'wb') as w:
     w.write(token[0]) # writing token in TOKEN.txt
 
 s.close()
