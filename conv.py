b09888a825c7a0c4f1c4cdeec3a58888f12bfb02
ashima-24 <jindal241995@gmail.com>
2018-11-14 10:52:51 +0100

    socket_cat

--- conv.py
+++ conv.py
@@ -11,12 +11,35 @@ with open ('conv.txt','wb') as f:
     while 1:
         msg=s.recv(1024)
         if not msg:
-                   break
+            break
         else:
-             data+=msg
+            data+=msg
              #print(msg)
-#             f.write(msg)
-file=open("conv.txt",'rb+')
-file.seek(56)
-file.write(b"62531")
+            f.write(msg)
+
+
+#print(data.split(b'\n')[0])
+#print(data.split(b'\n')[1])
+#print(data.split(b'\n')[2])
+b=data.split(b'\n')[3]
+img=b.split()[1]
+print(img)
+
+
+
+#print(img1[1])
+#print(data.split(b'TOKEN')[1])
+H=data.split(b'BYTES')
+t=H[1].split(b'TOKEN')
+#print(t[0])
+img1=bytearray(t[0])
+img3=img1.lstrip(b'\n')
+print(img3)
+with open ('cat.jpeg','wb') as i:
+   i.write(img3)
+tok=t[1].split(b'\n')
+token=tok[0].split(b':')
+#print(token[1])
+with open ('token.txt','wb') as w:
+    w.write(token[1])
 s.close()
